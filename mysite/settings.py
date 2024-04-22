46ef9101a27f6d722b7d5832f17434fb0a33b195
Arthur Calorio Martoneto <arthurcalorio@gmail.com>
2024-04-22 15:11:35 -0300

    Iniciando Projeto - Checkpoint

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'KIZpJmeRJeNPQowolGEVBfIBCmESghrQ',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '15976',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
